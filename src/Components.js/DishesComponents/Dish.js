1cc1df17c069200506a2c082a02f36d876202af2
JuliaRa411 <777.07j@gmail.com>
2022-11-05 00:07:14 -0400

    commit

--- src/Components.js/DishesComponents/Dish.js
+++ src/Components.js/DishesComponents/Dish.js
@@ -1,10 +1,17 @@
-const AllCategories =({dish}) => {
+import { useState } from "react";
+import ChangeQuantity from "../Cart/ChangeQuantity";
+
+const Dish =({dish}) => {
+  const [quantity, setQuantity] = useState (1);
     return (<div>
         <img src={`./${dish.img}.jpg`}  alt ="food"/>
         <h2> {dish.name}</h2> 
-        <h2> ${dish.price}</h2>
+        <p> ${dish.price}</p>
     
+    <ChangeQuantity quantity ={quantity} setQuantity ={setQuantity}/>
+
+    <button> Add to cart</button>
       </div>)
 }
 
-export default AllCategories
+export default Dish;
--- src/Components.js/Filter/AllCategories.js
+++ src/Components.js/Filter/AllCategories.js
@@ -1,7 +1,7 @@
 import Filter from "./Filter"
  
 const AllCategories = () =>{
-    return (<div>
+    return (<div className="contaimer">
         
         <h1> What kind of food do you like ? </h1> 
      {[ "SEAFOOD", "ITALIAN","APETIZERS", "SALADS", "ALL"]
